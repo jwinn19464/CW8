7fcc3110fa5ab043b543f5c9bd0d7edb0df98d80
jwinn19464 <jwinn19464@gmail.com>
2023-12-05 13:03:17 -0500

    fixed HelloWorld

--- my-app/src/HelloWorld.jsx
+++ my-app/src/HelloWorld.jsx
@@ -3,10 +3,8 @@ import React, { Component } from 'react';
 class HelloWorld extends Component {
     render() {
         return (
-            <Fragment>
-                <h1>Hello World!</h1>
-                <h1>Hello, {this.props.name}!</h1>
-            </Fragment>  
+                // <h1>Hello World!</h1>
+                <h1>Hello, {this.props.name}!</h1> 
         );
     }
 }
